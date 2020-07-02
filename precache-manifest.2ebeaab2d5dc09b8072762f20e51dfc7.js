4d0a02d121ccad56b8b068ad86732af7f7abac2b
NirikshaSKamath <59636330+NirikshaSKamath@users.noreply.github.com>
2020-07-02 11:56:12 +0530

    Updates

---
+++ precache-manifest.2ebeaab2d5dc09b8072762f20e51dfc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "246ebd190dad2268703c4286e1df3a0f",
+    "url": "/new-test/index.html"
+  },
+  {
+    "revision": "0ba09c66d3a2666c1860",
+    "url": "/new-test/static/css/main.e7fcc7b7.chunk.css"
+  },
+  {
+    "revision": "f3767212a34b6f773099",
+    "url": "/new-test/static/js/2.623ddddd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/new-test/static/js/2.623ddddd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ba09c66d3a2666c1860",
+    "url": "/new-test/static/js/main.f1a93547.chunk.js"
+  },
+  {
+    "revision": "a8f07e8710e3ed903bdc",
+    "url": "/new-test/static/js/runtime-main.ed836472.js"
+  }
+]);
